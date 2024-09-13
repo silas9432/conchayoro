2d887b563988cfc4a2cd7dac36b86f4ca1e4795b
Silas <silas9432@gmail.com>
2024-09-13 11:21:40 +0000

    Update frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   const black_friday = await blackFridayFlag();
   return (
       <div>
-        <h1> Welcome to the ConchaYOro App - deploy v3</h1>
+        <h1> Welcome to the ConchaYOro App - deploy v4</h1>
         { black_friday ? <button> Promo </button> : <button> Normal </button> }
       </div>
   )
